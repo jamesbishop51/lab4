a979434d1e7f011f926690e98fa8e811b9a33c38
James Bishop <S00187350@mail.itsligo.ie>
2019-10-10 12:46:31 +0100

    Add project files.

---
+++ question 1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Team> teams = new List<Team>(); 
+
+            Team SligoRovers = new Team ("Sligo Rovers");
+            Team FinnHarps = new Team ("Finn Harps");
+            Team GalwayUnited = new Team ("Galway United");
+            Team DerryCity = new Team ("Derry City");
+            Team Dundalk = new Team ("Dundalk");
+
+            teams.Add(SligoRovers);
+            teams.Add(FinnHarps);
+            teams.Add(GalwayUnited);
+            teams.Add(DerryCity);
+            teams.Add(Dundalk);
+
+            Display(teams);
+
+            SligoRovers.Addresult(Result.Win);
+            SligoRovers.Addresult(Result.Lose);
+            SligoRovers.Addresult(Result.Win);
+            FinnHarps.Addresult(Result.Lose);
+            FinnHarps.Addresult(Result.Lose);
+            FinnHarps.Addresult(Result.Draw);
+
+
+            Display(teams);
+
+
+
+        
+        }
+        
+        static void Display(List<Team> teams)
+        {
+
+            Console.WriteLine("\n{0,-15}{1,-7}{2,-7}{3,-7}{4,-7}{5,-7}","NAME","POINTS","WINS","DRAWS","LOSSES","GAMES PLAYED");
+
+            foreach (Team team in teams)
+            {
+                Console.WriteLine(team.DisplayTeamTable());
+            }
+        
+        }
+
+    }
+}
---
+++ question 1/Team.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_1
+{
+    public enum Result
+    {
+        Win,
+        Draw,
+        Lose
+
+    }
+    class Team
+    {
+
+        public string Teams { get; private set; }
+
+        public int NoOfWins { get; private set; }
+
+        public int NoOfDraws { get; private set; }
+
+        public int NoOflosses { get; private set; }
+
+        public int NoOfGames { get; private set; }
+
+        private int points;
+
+        public int Points
+        {
+            get
+            {
+                points = (NoOfWins * 3) + (NoOfDraws * 1);
+                return points;
+            }
+        }
+        public Team(string name)
+        {
+            Teams = name;
+        }
+
+
+        public string DisplayTeamTable()
+        {
+            return string.Format($"{Teams,-15}{Points,-7}{NoOfWins, -7}{NoOfDraws, -7}{NoOflosses,-7}{NoOfGames,-7}");
+        }
+
+        public void Addresult(Result result)
+        {
+            NoOfGames++;
+
+            switch (result)
+            {
+                case Result.Win:
+                    NoOfWins++;
+                    break;
+
+                case Result.Draw:
+                    NoOfDraws++;
+                    break;
+                case Result.Lose:
+                    NoOflosses++;
+                    break;
+
+
+            }
+        }
+
+    }
+    
+}
